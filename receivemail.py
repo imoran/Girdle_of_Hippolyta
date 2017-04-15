5ca03dd6bb895fadc78aaef5b3b8c5f102f7b688
Isis <imoran@mail.ccsf.edu>
2017-04-14 20:13:03 -0700

    finished the ability to send/receive messages

--- receivemail.py
+++ receivemail.py
@@ -2,7 +2,6 @@
 
 import imaplib
 import os
-from email.parser import HeaderParser
 
 user = 'isismoran93@gmail.com'
 password = '100607isis'
@@ -17,54 +16,10 @@ try:
     ids = data[0] # data is a list.
     id_list = ids.split() # ids is a space separated string
     latest_email_id = id_list[-1] # get the latest
-
     result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
-
     raw_email = data[0][1] # here's the body, which is raw text of the whole email
 
-    print raw_email
-
-# including headers and alternate payloads
+    message_content = raw_email[raw_email.find('Date'):]
+    print message_content
 except:
 	print "Sorry there was a problem connecting to your account"
-
-
-# mail = imaplib.IMAP4_SSL('imap.gmail.com')
-# mail.login('myemail@gmail.com', 'password123')
-# mail.list()
-# # Out: list of "folders" aka labels in gmail.
-# mail.select("inbox") # connect to inbox.
-#
-# result, data = mail.search(None, "ALL")
-#
-# ids = data[0] # data is a list.
-# id_list = ids.split() # ids is a space separated string
-# latest_email_id = id_list[-1] # get the latest
-#
-# result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
-#
-# raw_email = data[0][1] # here's the body, which is raw text of the whole email
-# # including headers and alternate payloads
-#
-#
-
-
-
-
-
-
-
-
-# result, data = mail.search(None, "ALL")
-#
-# ids = data[0] # data is a list.
-# id_list = ids.split() # ids is a space separated string
-# latest_email_id = id_list[-1] # get the latest
-#
-# result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
-#
-# raw_email = data[0][1] # here's the body, which is raw text of the whole email
-# # including headers and alternate payloads
-
-
-# search_criteria = 'REVERSE DATE'
